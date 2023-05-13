f7bea24e89f8ce14c45fff15f0edf91c10d86137
antonymuga <anthony.ochieng.muga@gmail.com>
2023-05-13 14:17:20 +0300

    Added project bones

---
+++ execmd.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+void execmd(char **argv)
+{
+    char *command = NULL, *actual_command = NULL;
+    pid_t child_pid;
+    int status;
+
+    if (argv)
+    {
+        /* get the command */
+        command = argv[0];
+
+        /* generate the path to this command before passing it to execve */
+        actual_command = get_location(command);
+        
+        /* create child process */
+        
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+            exit(EXIT_FAILURE);
+        }
+        /* execute the actual command with execve */
+        if (child_pid == 0)
+        {
+            if (execve(actual_command, argv, NULL) == -1)
+            {
+                perror("Error:");
+            }
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
+    
+}

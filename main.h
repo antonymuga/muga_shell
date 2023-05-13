33d51f0b934ece0d8a57dd814c08afbfd1e45b9d
antonymuga <anthony.ochieng.muga@gmail.com>
2023-05-13 14:23:02 +0300

    Updated

--- main.h
+++ main.h
@@ -1,9 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 void execmd(char **argv);
 char *get_location(char *command);
+int stat(const char *pathname, struct stat *statbuf);
+
+#endif

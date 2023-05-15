9784e6888877167ed60e260f1448326e078576dc
antonymuga <anthony.ochieng.muga@gmail.com>
2023-05-15 11:07:05 +0300

    core dumped

--- replace_variables.c
+++ replace_variables.c
@@ -4,29 +4,38 @@ void replace_variables(char **argv)
 {
     int i;
     char pid_str[10];
+    char *env_val;
 
     for (i = 0; argv[i] != NULL; i++)
     {
-        /* check if the argument starts with $ */
-        if (argv[i][0] == '$')
+    /* check if the argument starts with $ */
+    if (argv[i][0] == '$')
+    {
+        /* check if it's the $$ variable */
+        if (strcmp(argv[i], "$$") == 0)
         {
-            /* check if it's the $$ variable */
-            if (strcmp(argv[i], "$$") == 0)
+            sprintf(pid_str, "%d", getpid());
+            free(argv[i]);
+            argv[i] = strdup(pid_str);
+        }
+        /* check if it's the $? variable */
+        else if (strcmp(argv[i], "$?") == 0)
+        {
+            env_val = getenv("?");
+            free(argv[i]);
+            if (env_val != NULL)
             {
-                sprintf(pid_str, "%d", getpid());
-                argv[i] = pid_str;
+                argv[i] = strdup(env_val);
             }
-            /* check if it's the $? variable */
-            else if (strcmp(argv[i], "$?") == 0)
-            {
-                argv[i] = getenv("?");
-            }
-            /* handle other variables here */
             else
             {
-                /* ... */
+                argv[i] = strdup("");
             }
         }
+        /* handle other variables here */
+        else
+        {
+            /* ... */
+        }
     }
-    free(i);
 }
